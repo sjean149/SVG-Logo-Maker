2f9f8ead8c871cb6434a8262f2f2b0bb389bb71e
Sephora Jean <sephorejean@gmail.com>
2024-06-20 12:01:49 -0400

    Program can generate html or svg files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SVG-Logo-Maker
+
+#This program takes in user input to create a logo. 
+
+It is run using index.js. All the user has to do is input logoText, textColor, shape, shapeColor, and their preferred fileType and the program will create an instance of the coressponding class that will generate either an SVG or HTML file of the logo. 
---
+++ index.js
@@ -0,0 +1,100 @@
+//import inquirer from 'inquirer';
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { type } = require('os');
+const Shapes = require('./lib/shapes');
+
+const Circle = require('./lib/circle');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
+
+let logoLetters;
+let fileType;
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'logoLetters',
+            message: "Enter up to three characters",
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "What color would you like the text to be?",
+        },
+        {
+            type: "list",
+            name: "shape",
+            message: "Choose one of the following shapes for your logo",
+            choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "What color would you like your shape to be?",
+
+        },
+        {
+            type: "list",
+            name: "fileType",
+            message: "Choose one of the following two file types for your logo",
+            choices: ["HTML", "SVG"],
+        }
+
+
+    ]).then(data => {
+        let currentLogo;
+        fileType = data.fileType;
+
+        if (data.shape == 'Circle') {
+            currentLogo = new Circle(data.logoLetters, data.textColor, data.shape, data.shapeColor, data.fileType);
+        } else if (data.shape == 'Triangle') {
+            currentLogo = new Triangle(data.logoLetters, data.textColor, data.shape, data.shapeColor, data.fileType);
+        } else {
+            currentLogo = new Square(data.logoLetters, data.textColor, data.shape, data.shapeColor, data.fileType);
+        }
+
+
+        //creates html and svg content for files
+        if (data.fileType == 'HTML') {
+            htmlLogo = currentLogo.renderHTML();
+            return htmlLogo;
+        } else if (data.fileType == 'SVG') {
+            svgLogo = currentLogo.renderSVG()
+            return svgLogo;
+        } return;
+
+
+    }).then(data => {
+        
+        console.log("Second .then ", data);
+        console.log("data.filetupe", fileType);
+
+        //deletes logo html file if one exists and user chooses to make html file
+        if (fileType == 'HTML') {
+            if (fs.existsSync('logo.html')) {
+                fs.unlinkSync('logo.html');
+            }
+
+            fs.writeFileSync(`./generatedFiles/logo.html`, data, err => {
+                console.error("there was an error", err);
+            });
+        };
+
+
+        //deletes logo svg file if one exists and user chooses to make svg file
+        if (fileType == 'SVG') {
+            if (fs.existsSync('logo.svg')) {
+                fs.unlinkSync('logo.svg');
+            }
+
+            fs.writeFileSync(`./generatedFiles/logo.svg`, data, err => {
+                console.error("there was an error", err);
+            });
+        };
+
+
+
+
+    });
